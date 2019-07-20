232e0934ac31ea5fb8f4edfbe28f9ff4cb533b50
hayataka000 <hysdtksg@gmail.com>
2019-07-20 17:19:05 +0900

    sign_out ルート設定

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,11 @@ Rails.application.routes.draw do
 
   get '/confirm/index'
   devise_for :users
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
   resources :cards, only: [:index] do
   end
   resources :mypages,only:[:index]
