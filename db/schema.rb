07da4cacd2bcf42bbce4deefc8806d5da17ce2be
shohei yamada <saka1212vn@gmail.com>
2019-07-23 15:15:34 +0900

    プルリク指摘されていた件修正しました。

--- db/schema.rb
+++ db/schema.rb
@@ -37,17 +37,15 @@ ActiveRecord::Schema.define(version: 2019_07_21_123852) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.text "name"
-    t.text "name_furigana", null: false
-    t.text "password", null: false
-    t.string "mail", default: "", null: false
+    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["mail"], name: "index_users_on_email", unique: true
+    t.string "name"
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
