6b6c7ebbfb1969ebab0c1ea28746e147ad67b68d
西村勇人 <nishimurahayato@nishimurahayatonoMacBook-Air.local>
2019-07-25 17:35:25 +0900

    変更

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -7,7 +7,11 @@ class ItemController < ApplicationController
     @item = Item.new
   end
   def create
+    Item.create(item_params)
   end
-
+  private 
+  def item_params
+      params.permit(:name, :image,:price,:size,:condition,:delivery_fee,:delivery_date,:delivery_method,:shipping_area,:content,:category)
+  end
 end
 
