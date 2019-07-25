e56214ce646523d8c2616873be2100b09813eec8
shohei yamada <saka1212vn@gmail.com>
2019-07-25 15:55:35 +0900

    プルリク訂正(途中)

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -6,7 +6,7 @@ class ItemController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
     @item = Item.find(params[:id])
+    @user = User.find(params[:id])
   end
 end
