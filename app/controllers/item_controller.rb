acb8122990483e14bf7088a0c7b2d81d45fe4138
mikiji13 <mikiji13kakinoki@gmail.com>
2019-07-25 18:34:42 +0900

    中間

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -9,7 +9,7 @@ class ItemController < ApplicationController
   def create
      Item.create(item_params)
   end
-  
+
   private
   def item_params
     params.permit(:name, :iamge, :price, :size, :condition, :delivery_fee, :delivery_fee, :delivery_date, :delivery_method, :shipping_area, :content)
