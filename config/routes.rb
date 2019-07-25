9b25d583c1389ecc07897b8773b3824986a70804
hayataka000 <hysdtksg@gmail.com>
2019-07-25 20:50:08 +0900

    SNS認証 設定記述

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,8 @@ Rails.application.routes.draw do
   get '/registrations/new' => 'registrations#new'
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
-    :sessions => 'users/sessions'
+    :sessions => 'users/sessions',
+    :omniauth_callbacks => 'users/omniauth_callbacks'
   }
 
   devise_scope :user do
