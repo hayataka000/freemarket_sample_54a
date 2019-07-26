06018387c0911d8aa4e627407ac6672fae03635d
hayataka000 <hysdtksg@gmail.com>
2019-07-26 14:06:17 +0900

    omniauth 設定

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -18,17 +18,19 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   # common callback method
   def callback_for(provider)
+    provider = provider.to_s
     @user = User.from_omniauth(request.env["omniauth.auth"])
+
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
     else
-      session["devise.#{provider}_data"] = request.env["omniauth.auth"].except("extra")
-      redirect_to new_user_registration_url
+      session[:nickname] = @user.nickname
+      session[:email] = @user.email
+      session[:password] = @user.password
+      session[:provider] = @user.provider
+      session[:uid] = @user.uid
+      redirect_to new_user_registration_sns_path
     end
-  end
-
-  def failure
-    redirect_to root_path
   end
 end
