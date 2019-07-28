1b2f2668af9faa524bc3a83cb48d7bc8c3bf789b
西村勇人 <nishimurahayato@nishimurahayatonoMacBook-Air.local>
2019-07-28 16:29:26 +0900

    微修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,16 @@
 class User < ApplicationRecord
+  has_many :items
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable,omniauth_providers: %i[facebook google_oauth2]
   def self.from_omniauth(auth)
-      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-        user.email = auth.info.email
-        user.password = Devise.friendly_token[0,20]
-      end
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
     end
   end
 
   private
-
   def self.dummy_email(auth)
     "#{auth.uid}-#{auth.provider}@example.com"
   end
-  has_many :items
 end
