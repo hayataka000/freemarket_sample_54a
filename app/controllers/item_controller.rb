9fa9704da7a37abc9c877f3bd6cb4c386a607256
shohei yamada <saka1212vn@gmail.com>
2019-07-28 17:31:14 +0900

    編集機能実装

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -7,40 +7,26 @@ class ItemController < ApplicationController
     @item = Item.new
   end
   def create
-    Item.create(item_params)
-    @item.save
   end
-
-
- 
-  
-  def edit
-    @item = Item.find(params[:id])
-  end
-
-  
-  def update
-    @item = Item.find(params[:id])
-    @item.update(params.require(:item).permit(:name, :image,:price,:size,:condition,:delivery_fee,:delivery_date,:delivery_method,:shipping_area,:content,:category))
-    redirect_to item_index_path
-  end
-
-  private 
-  def item_params
-      params.permit(:name, :image,:price,:size,:condition,:delivery_fee,:delivery_date,:delivery_method,:shipping_area,:content,:category)
-  end
-
-  private
-  def item_params
-    params.require(:item).permit(:name, :image, :price, :size, :condition, :delivery_fee, :delivery_date, :delivery_method, :shipping_area, :content, :category)
-
      @item = Item.new(item_params)
      if @item.save
-      binding.pry
       redirect_to root_path
     else
       render :new
   end
+end
+
+
+def edit
+  @item = Item.find(params[:id])
+end
+
+
+def update
+  @item = Item.find(params[:id])
+  @item.update(item_params)
+  binding.pry
+  redirect_to item_index_path
 end
 
   private 
@@ -49,7 +35,6 @@ end
   def show
     @item = Item.find(params[:id])
     @user = @item.user
-
   end
   
   def item_params
