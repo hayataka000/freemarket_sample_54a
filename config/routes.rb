0542ae58f69f32bac1ccb71156b121b8c79df1b8
tatsuya-koyam26 <koyama.tatsuya@icloud.com>
2019-07-30 19:25:38 +0900

    ルーティングにidを追記

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :cards, only: [:index, :new]
 
   post '/done'=> "item#_done"
-  get '/product_purchase_confirmation'=> "item#_product_purchase_confirmation"
+  get '/product_purchase_confirmation/:id'=> "item#product_purchase_confirmation"
   get '/item/new', to: 'item#new'
   get '/confirm/index'
   get '/registrations/new' => 'registrations#new'
