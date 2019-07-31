000b9f7c612a50d413e73d39c01482ed869eace8
hayataka000 <hysdtksg@gmail.com>
2019-07-31 21:32:25 +0900

    バリデーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,12 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 belongs_to :user,optional:true
-belongs_to :item,optional:true
 belongs_to_active_hash :prefecture,optional:true
 belongs_to_active_hash :delivery_fee,optional:true
 has_one_attached :image
 belongs_to_active_hash :category
 
-
+validates :name, :price, :condition, :delivery_date, :content, :image, presence: true
+validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 
 end
