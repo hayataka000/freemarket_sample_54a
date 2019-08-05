36ec3e1d453032c5a8a14e13be19293c14d54638
shohei yamada <saka1212vn@gmail.com>
2019-08-05 13:45:47 +0900

    商品削除後トップページ行くように変更

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -36,6 +36,7 @@ skip_before_action :authenticate_user!, only: [:show,:index]
   def destroy
     if @item.user_id == current_user.id
       @item.destroy
+      redirect_to root_path
     end
   end
 
